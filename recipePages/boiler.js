0ad409e318349ad23e0df73f377847820cf2d435
hxrish <108225503+hxrish@users.noreply.github.com>
2025-03-05 10:23:54 +0530

    Use boilerplate

--- recipePages/boiler.js
+++ recipePages/boiler.js
@@ -1,6 +1,12 @@
 const closeBtn = document.querySelectorAll('.closeBtn');
 const popup  = document.querySelectorAll('.imageOverlay');
+const viewImage = document.querySelectorAll('.imageContainer');
 
 closeBtn.forEach(closeBtn => closeBtn.addEventListener('click', () => {
     popup.forEach(popup => popup.style.display = "none")
+}))
+
+viewImage.forEach(viewImage => viewImage.addEventListener('click', ()=>{
+    event.preventDefault();
+    popup.forEach(popup => popup.style.display = "flex")
 }))
